947a79cd125320086eea916cac3adaf97a9f2f25
Arsdev998 <arsdev998@gmail.com>
2025-01-30 12:40:34 +0700

    test deploy 3

--- src/server.js
+++ src/server.js
@@ -1,15 +1,4 @@
-import "dotenv/config";
 import app from "./app.js";
-import connect from "./config/connection.js";
+import { createServer } from "@vercel/node";
 
-
-connect()
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  
-});
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default createServer(app);
